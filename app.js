4e3128caaf4780db1ec243e0d4456b8a04385ebe
Ismo Mehdi <84803085+ismomehdi@users.noreply.github.com>
2025-03-24 18:48:12 +0200

    fix thing

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express()
 // eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 5000
 
-app.use(s('dist'))
+app.use(express.static('dist'))
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
